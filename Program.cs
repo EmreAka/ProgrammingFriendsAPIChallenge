ffe52670034549be799012aec2c5e25257201097
Emre <aka548548@gmail.com>
2023-03-03 20:19:06 +0300

    API for Programming Friends challenge.

--- Program.cs
+++ Program.cs
@@ -5,15 +5,16 @@ app.MapGet("/", () => "Hello Programming Friend ^^ To test the api make a post r
 app.MapPost("/challenge", (Request request) => {
     BiggestNumber biggestNumber = new BiggestNumber(0,0);
     BiggestNumber biggestSecondNumber = new BiggestNumber(0,0);
+    var distinctNumbers = request.Numbers.Distinct();
     
-    foreach (var number in request.Numbers.Distinct()){
+    foreach (var number in distinctNumbers){
       var result = request.Numbers.Count(x => x == number);
       if(biggestNumber.Count < result){
         biggestNumber = new(number, result);
       }
     }
 
-    foreach (var number in request.Numbers.Distinct().Where(x => biggestNumber.Number != x)){
+    foreach (var number in distinctNumbers.Where(x => biggestNumber.Number != x)){
       var result = request.Numbers.Count(x => x == number);
       if(biggestSecondNumber.Count < result){
         biggestSecondNumber = new(number, result);
